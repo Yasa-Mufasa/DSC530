ea2c4743be8ee7fe7e746e392b233aebad3b3882
YasaMufasa <43527639+YasaMufasa@users.noreply.github.com>
2018-12-04 20:28:53 -0700

    Uploading assignment details and submission

---
+++ Week2/excersize_2_1.py
@@ -0,0 +1,59 @@
+'''
+DSC 530 - T301 Data Exploration and Analysis
+Exercise 2.1: Preparing for Exploratory Data Analysis
+'''
+
+'''
+Display the text "Hello World! I wonder why that is always the default coding
+text to start with
+'''
+question1 = "Hello World! I wonder why that is always the default coding text to start with"
+print(question1)
+
+'''
+Add two numbers together
+'''
+print(1+5)
+
+'''
+Subtract a number from another number
+'''
+print(10-15)
+
+'''
+Multiply two numbers
+'''
+print(7 * 6)
+
+'''
+Divide between two numbers
+'''
+# I'm going to do division using both ints and floats to display the differences
+print(5/3)
+print(5.0/3)
+print(round(5.0/3,3))
+
+'''
+Concatenate two strings together (any words)
+'''
+word1 = "any "
+word2 = 'words'
+print(word1 + word2)
+print('any' + 'words')
+
+'''
+create a list of 4 items (can be strings, numbers, both)
+'''
+randomness = [42, 'So long and thanks for all the fish', 'resistance is useless', 'improbability']
+print(randomness)
+
+'''
+append an item to your list (again, can be a string, number)
+'''
+randomness.append("Don't Panic")
+print(randomness)
+'''
+Create a tuple with 4 items (can be strings, numbers, both)
+'''
+tup = (1, 'test', 3, 'random')
+print(tup)
