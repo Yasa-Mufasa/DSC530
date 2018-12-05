4032093638d26ca5ca3ca49f51712627d43b4b85
YasaMufasa <43527639+YasaMufasa@users.noreply.github.com>
2018-12-04 22:03:34 -0700

    Adding formatting for when the code is run

--- Week2/exercise_2_1.py
+++ Week2/exercise_2_1.py
@@ -7,35 +7,46 @@ Exercise 2.1: Preparing for Exploratory Data Analysis
 Display the text "Hello World! I wonder why that is always the default coding
 text to start with
 '''
+print('Display the text "Hello World! I wonder why that is always the default coding text to start with')
 question1 = "Hello World! I wonder why that is always the default coding text to start with"
 print(question1)
 
 '''
 Add two numbers together
 '''
+print('Add two numbers together')
+print('1 + 5')
 print(1+5)
 
 '''
 Subtract a number from another number
 '''
+print('Subtract a number from another number')
+print('10 - 15')
 print(10-15)
 
 '''
 Multiply two numbers
 '''
+print('Multiply two numbers')
+print('7 * 6')
 print(7 * 6)
 
 '''
 Divide between two numbers
 '''
 # I'm going to do division using both ints and floats to display the differences
+print('Divide between two numbers')
+print('5/3')
 print(5/3)
+print('5.0/3')
 print(5.0/3)
-print(round(5.0/3,3))
 
 '''
 Concatenate two strings together (any words)
 '''
+print('Concatenate two strings together (any words)')
+print("any " + "words")
 word1 = "any "
 word2 = 'words'
 print(word1 + word2)
@@ -44,16 +55,20 @@ print('any' + 'words')
 '''
 create a list of 4 items (can be strings, numbers, both)
 '''
+print('create a list of 4 items (can be strings, numbers, both)')
 randomness = [42, 'So long and thanks for all the fish', 'resistance is useless', 'improbability']
 print(randomness)
 
 '''
 append an item to your list (again, can be a string, number)
 '''
+print('append an item to your list (again, can be a string, number)')
 randomness.append("Don't Panic")
 print(randomness)
+
 '''
 Create a tuple with 4 items (can be strings, numbers, both)
 '''
+print('Create a tuple with 4 items (can be strings, numbers, both)')
 tup = (1, 'test', 3, 'random')
 print(tup)
